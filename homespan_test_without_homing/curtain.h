2d4041bf1873a6b750bdda2c7cbdcbb497e4cff5
Tim <www.timmilo@gmail.com>
2022-06-04 00:54:08 +0200

    add reference run

--- homespan_test_without_homing/curtain.h
+++ homespan_test_without_homing/curtain.h
@@ -2,27 +2,45 @@
 //  control of the curtain    //
 ////////////////////////////////
 
-const byte dirPin = 21;             // pin connected to DIR pin of Easy tmc
-const byte stepPin = 19;            // pin connected to STEP pin of Easy tmc
-const byte motorEnPin = 23;            // motor sleep pin
+const byte dirPin = 21;                               // pin connected to DIR pin of Easy tmc
+const byte stepPin = 19;                              // pin connected to STEP pin of Easy tmc
+const byte motorEnPin = 23;                           // motor sleep pin
+const byte diagPin = 26;                              // diag pin from TMC 2209
 
-const int movementSpeed = 4500;    // motor speed
-const int movementAccel = 3500;    // motor acceleration
+const int movementSpeed = 4500;                       // motor speed
+const int referenceSpeed = 1000;                      // motor speed for the reference run
+const int movementAccel = 3500;                       // motor acceleration
 
-unsigned long posEnd = 10000;       // end position for testing set to a fixed position
-unsigned long pos;                  // current curtain position
+const bool shaftDirection = true;                     // to reverse the motor direction
 
+const uint8_t driverAddress = 0b00;                   // TMC2209 Driver address according to MS1 and MS2
+const float rSense = 0.11f;                           // E_SENSE for current calc.
+const unsigned int stallValue = 30;                   // [0..255] SGTHRS change if stall is wrong
 
-AccelStepper stepper(1, stepPin, dirPin); // 1 = Easy tmc interface
-  
+unsigned long posEnd = 10000;                         // end position for testing set to a fixed position
+unsigned long pos;                                    // current curtain position
+
+bool stallGuard = false;
+
+#include <AccelStepper.h>
+#include <TMCStepper.h>
+
+TMC2209Stepper driver(&Serial2, rSense, driverAddress);
+
+AccelStepper stepper(1, stepPin, dirPin);             // 1 = Easy driver interface
+
+void IRAM_ATTR diag() {
+  stallGuard = true;
+} 
+
 struct DEV_curtain : Service::WindowCovering{
 
-  SpanCharacteristic *curPos;   // CurrentPosition      uint8_t
-  SpanCharacteristic *tarPos;   // TargetPosition       uint8_t
-  SpanCharacteristic *posSta;   // PositionState        uint8_t   | 0=closing | 1=opening | 2=stopped |
-  //SpanCharacteristic *obsDet;   // ObstructionDetected  boolean
+  SpanCharacteristic *curPos;                         // CurrentPosition      uint8_t
+  SpanCharacteristic *tarPos;                         // TargetPosition       uint8_t
+  SpanCharacteristic *posSta;                         // PositionState        uint8_t   | 0=closing | 1=opening | 2=stopped |
+  //SpanCharacteristic *obsDet;                         // ObstructionDetected  boolean
 
-  DEV_curtain() : Service::WindowCovering(){  // constructor() method
+  DEV_curtain() : Service::WindowCovering(){          // constructor() method
     
     curPos = new Characteristic::CurrentPosition(0);
     tarPos = new Characteristic::TargetPosition();
@@ -33,8 +51,8 @@ struct DEV_curtain : Service::WindowCovering{
   } // end constructor
 
   boolean update(){
-    stepper.setMaxSpeed(movementSpeed);              // set movment speed
-    stepper.setAcceleration(movementAccel);          // set acceleration  
+    stepper.setMaxSpeed(movementSpeed);               // set movment speed
+    stepper.setAcceleration(movementAccel);           // set acceleration  
 
     if(tarPos->updated()){
       if(tarPos->getNewVal() > curPos->getVal()){     // if the target-position requested is greater than the current-position  
@@ -46,14 +64,14 @@ struct DEV_curtain : Service::WindowCovering{
         LOG1("Closing  curtain\n");
       }
       
-      digitalWrite(motorEnPin, LOW);                  // enable motor
+      driver.toff(4);                                 // enable motor
       pos = posEnd*(tarPos->getNewVal());             // calculate new target position
       pos = pos/100;
 
       stepper.moveTo(pos);                            // set new target positon
       stepper.runToPosition();                        // move stepper to the new position
       
-      digitalWrite(motorEnPin, HIGH);                 // disable motor
+      driver.toff(0);                                 // disable motor
       curPos->setVal(pos*100/posEnd);                 // set curtain position to homekit
       posSta->setVal(0);
       
@@ -68,8 +86,21 @@ struct DEV_curtain : Service::WindowCovering{
 
 
 void stepper_reference_run() {
-  LOG0("\n");
-  LOG0("TODO: stepper_reference_run !!!!");
-  LOG0("\n");
-  stepper.setCurrentPosition(0);    // for test purpos
+  Serial.println("Reference run !");
+  attachInterrupt(diagPin, diag, RISING);             // activate interrupt
+  
+  driver.toff(4);                                     // enable motor
+  
+  while(stallGuard == false){
+    driver.VACTUAL(-1000);
+  }
+  driver.VACTUAL(0);
+  stallGuard = true;
+  
+  
+  stepper.setCurrentPosition(-16);
+  Serial.print("Reference run complete !");
+
+  driver.toff(0);                                     // disable motor
+  detachInterrupt(diagPin);                           // deactivate interrupt
 }
--- homespan_test_without_homing/homespan_test_without_homing.ino
+++ homespan_test_without_homing/homespan_test_without_homing.ino
@@ -1,11 +1,22 @@
-//-------------------------
-//     ESP32 DEV Modul
-//-------------------------
+////////////////////////////////
+//      ESP32 DEV Modul       //
+////////////////////////////////
 
-#define SkeVer "0.9.7"                // Software version
-#define HardVer "0.0.0"               // Hardware version
+#define curtain
+#define blinds
 
-#include <AccelStepper.h>
+
+#define homekit_code 46637726 // 46637726
+
+#define auto_start_ap
+
+#define homeSpan_ota
+
+#define battery_mode
+
+
+#define sketch_version "0.9.7"        // Software version
+#define hardware_version "0.0.0"      // Hardware version
 
 #include "HomeSpan.h"
 
@@ -16,21 +27,34 @@ const byte statusLED = 15;            // HomeSpan status LED
 
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(500000);
+  Serial2.begin(256000);
 
   pinMode(motorEnPin, OUTPUT);        // motor sleep pin
   pinMode(batPin, INPUT);             // battery analg pin
-  
-  //digitalWrite(motorEN, HIGH);
-  
-  digitalWrite(motorEnPin, LOW);      // enable motor (current in hardware)
-  stepper_reference_run();            // starts a reference run
-  digitalWrite(motorEnPin, HIGH);     // disable motor (current in hardware)
+
+  digitalWrite(motorEnPin, LOW);      // enable motor in hardware
+    
+  driver.begin();
+  driver.toff(0);                     // disable motor
+  driver.blank_time(24);
+  driver.rms_current(200); 
+  driver.microsteps(8);
+  driver.TCOOLTHRS(0x4E2);            // 20bit max //0xFFFFF
+  //driver.seimin(1);
+  driver.semin(0);//4
+  driver.semax(2);//2
+  driver.shaft(shaftDirection);
+  driver.sedn(0b01);
+  //driver.pwm_autoscale(true);
+  driver.SGTHRS(stallValue);
+
+  //stepper_reference_run();            // starts a reference run
+
 
   //homeSpan.setControlPin(pin);        // set the control button
   homeSpan.setStatusPin(statusLED);   // set the status LED for HomeSpan
   homeSpan.setStatusAutoOff(60);      // sets Status LED to automatically turn off after duration seconds
-  homeSpan.setSketchVersion(SkeVer);  // set the sketch version
+  homeSpan.setSketchVersion(sketch_version);  // set the sketch version
   //homeSpan.setApSSID("");             // sets the SSID (network name) of the HomeSpan Setup Access Point
   //homeSpan.setApPassword("");         // sets the password of the HomeSpan Setup Access Point
   //homeSpan.setQRID("HSPN");           // changes the Setup ID(4), which is used for pairing a device with a QR Code, the HomeSpan default is "HSPN"
@@ -47,8 +71,8 @@ void setup() {
       new Characteristic::Manufacturer("HomeSpan");
       new Characteristic::SerialNumber("1123581321");
       new Characteristic::Model("OSSCR 2.0");
-      new Characteristic::FirmwareRevision(SkeVer);
-      new Characteristic::HardwareRevision(HardVer);
+      new Characteristic::FirmwareRevision(sketch_version);
+      new Characteristic::HardwareRevision(hardware_version);
 
       new DEV_curtain();
 
